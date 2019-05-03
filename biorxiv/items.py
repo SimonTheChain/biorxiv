6fd2aeeb08cbb2b4804bace6918fdd3334cee8c6
Simon Lachaîne <simonthechain@gmail.com>
2019-05-03 09:23:42 -0400

    Removed pdf link input processor

--- biorxiv/items.py
+++ biorxiv/items.py
@@ -10,22 +10,7 @@ import re
 
 import scrapy
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import MapCompose
 from scrapy.loader.processors import TakeFirst
-
-
-def extract_link(text):
-    """
-    Extracts a link from a string
-    :param text: String containing a url
-    :return: Link as string
-    """
-    # reference: https://daringfireball.net/2009/11/liberal_regex_for_matching_urls
-    pattern = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
-    link = pattern.match(text)
-
-    if link:
-        return link.group(0)
 
 
 class ArticleItem(scrapy.Item):
@@ -33,9 +18,7 @@ class ArticleItem(scrapy.Item):
     Structures data found in articles
     """
     title = scrapy.Field()
-    pdf_link = scrapy.Field(
-        input_processor=MapCompose(extract_link)
-    )
+    pdf_link = scrapy.Field()
     abstract = scrapy.Field()
     copyright_info = scrapy.Field()
     authors = scrapy.Field()
@@ -53,6 +36,6 @@ class AuthorItem(scrapy.Item):
 
 class ArticleItemLoader(ItemLoader):
     """
-
+    Applies TakeFirst to all item outputs
     """
     default_output_processor = TakeFirst()
